0051240f2b4cb140ba7fdac942159f66084d2108
shoaibhajj09 <shoaibhajj09@gmail.com>
2025-03-27 12:41:40 +0300

    adding Analytics

---
+++ layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/Starbackground";
+import NavBar from "@/components/NavBar";
+import { Analytics } from "@vercel/analytics/react";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Space Poftfolio",
+  description: "Shoaib Hajj Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <NavBar />
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
